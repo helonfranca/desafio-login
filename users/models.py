2d914c5267dd78bd4d2b1e0b0490d4fa63f305e7
Helon de França <92336676+helonfranca@users.noreply.github.com>
2025-02-25 21:42:02 -0300

    fix: definindo email como atributo de autenticação

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)  # E-mail único obrigatório
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  
 
     def __str__(self):
         return self.username
